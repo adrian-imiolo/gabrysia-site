77d8f9a2f417ae5d7944d1e0b2eb0e9af330a63b
Adrian Imioło <adrian.imiolo11@gmail.com>
2023-12-08 23:00:36 +0100

    Created services

--- src/components/Navbar/Services.tsx
+++ src/components/Navbar/Services.tsx
@@ -1,23 +1,91 @@
 function Services() {
   return (
     <section className="services">
-      <div className="service">
-        <h3>Prawo cywilne</h3>
-      </div>
-      <div className="service">
-        <h3>Prawo rodzinne oraz międzynarodowe prawo rodzinne</h3>
-      </div>
-      <div className="service">
-        <h3>Prawo pracy</h3>
-      </div>
-      <div className="service">
-        <h3>Prawo karne</h3>
-      </div>
-      <div className="service">
-        <h3>Prawo administracyjne</h3>
-      </div>
-      <div className="service">
-        <h3>Prawo gospodarcze</h3>
+      <h2>Główne obszary działania</h2>
+      <div className="services-container">
+        <div className="services-box">
+          <h3>Prawo cywilne</h3>
+          <p>Jednym z głównych obszarów moich zainteresowań oraz praktyki prawniczej jest szeroko
+rozumiane prawo cywilne. Prowadzę spory sądowe, ale także zajmuję się bieżącym doradztwem,
+polegającym na sporządzaniu opinii prawnych czy przygotowywaniu umów.</p>
+<br />
+          <p>W szczególności są to sprawy:
+          &bull; o zapłatę
+          &bull; o wykonanie umów oraz sprawy związane z nienależytym ich wykonaniem
+          &bull; konsumenckie
+          &bull; o odszkodowanie i zadośćuczynienie, w tym również sprawy dotyczące wypadków
+komunikacyjnych
+&bull; o ochronę dóbr osobistych
+&bull; spadkowe (stwierdzenie nabycia spadku, dział spadku, zachowek, ważność testamentu)
+&bull; o ochronę prawa własności
+&bull; z zakresu współwłasności, w tym jej zniesienia
+&bull; dotyczące kwestii zasiedzenia</p>
+        </div>
+        <div className="services-box">
+          <h3>Prawo rodzinne oraz międzynarodowe prawo rodzinne</h3>
+          <p>Kolejnym, jednym z głównych obszarów mojej praktyki są sprawy z zakresu prawa rodzinnego.
+Prowadzę również sprawy dotyczące międzynarodowego prawa rodzinnego i udzielam porad
+prawnych w tym zakresie obcokrajowcom.</p>
+<br />
+<p>W szczególności są to następujące sprawy:
+&bull; rozwód i separacja
+&bull; podział majątku wspólnego małżonków
+&bull; ustanowienie rozdzielności majątkowej małżeńskiej
+&bull; władza rodzicielska (ograniczenie, pozbawienie, zawieszenie i przywrócenie)
+&bull; zastępcza zgoda sądu opiekuńczego
+&bull; alimentacja (ustalenie, uchylenie, obniżenie, podwyższenie)
+&bull; kontakty z dzieckiem (ustalenie, zmiana, zakazanie)
+&bull; ustalenie i zaprzeczenie pochodzenia dziecka</p>
+        </div>
+        <div className="services-box">
+          <h3>Prawo pracy</h3>
+          <p>Prowadzę również sprawy z zakresu prawa pracy, na etapie przedsądowym i sądowym, świadcząc
+pomoc prawną zarówno pracownikom, jak i pracodawcom.</p>
+<br />
+<p>
+W szczególności są to następujące sprawy:
+
+&bull; o zapłatę zaległego wynagrodzenia i ekwiwalentu za niewykorzystany urlop
+&bull; o przywrócenie do pracy
+&bull; o uznanie wypowiedzenia za bezskuteczne i zapłatę odszkodowania
+&bull; o sprostowanie świadectwa pracy
+&bull; o ustalenie wypadku przy pracy
+&bull; mobbing
+</p>
+        </div>
+        <div className="services-box">
+          <h3>Prawo karne</h3>
+          <p>Posiadam również doświadczenie w prowadzeniu sprawach wykroczeniowych oraz karnych w
+każdym stadium postępowania, tj na etapie postępowania przygotowawczego, sądowego oraz
+wykonawczego.</p>
+<br />
+<p>W szczególności zajmuję się:
+&bull; obroną podejrzanego na etapie postępowania przygotowawczego (zatrzymanie, przedstawienie
+zarzutów, tymczasowe aresztowanie oraz pozostałe środki zapobiegawcze, branie udziału w
+czynnościach prowadzonych w toku dochodzenia lub śledztwa, składanie wniosków dowodowych)
+&bull; obroną oskarżonego na dalszym etapie postępowania
+&bull; obroną skazanego na etapie postępowania wykonawczego (sprawy dotyczące wstrzymania i
+odroczenia wykonania kary, przerwy w wykonaniu kary, warunkowe przedterminowe zwolnienie z
+odbycia reszty kary, o odbywanie kary w systemie dozoru elektronicznego)
+&bull; sprawami o wydanie wyroku łącznego
+&bull; reprezentacją pokrzywdzonych (oskarżycieli</p>
+        </div>
+        <div className="services-box">
+          <h3>Prawo administracyjne</h3>
+          <p>Posiadam doświadczenie w prowadzeniu sprawa przed organami władzy publicznej oraz sądami
+administracyjnymi. Sporządzam odwołania od decyzji administracyjnych, skargi do wojewódzkich
+sądów administracyjnych oraz skargi kasacyjne do Naczelnego Sądu Administracyjnego. Obszar mojej
+praktyki w tej dziedzinie prawa obejmuje w szczególności prawo budowlane (m.in. uzyskanie
+pozwolenia na budowę, postępowanie legalizacyjne) oraz sprawy dot. cudzoziemców (m.in.
+legalizacja pobytu).</p>
+        </div>
+        <div className="services-box">
+          <h3>Prawo gospodarcze</h3>
+          <p>Świadczę pomoc prawną przedsiębiorcom prowadzącym jednoosobową działalność gospodarczą, jak
+i spółkom prawa handlowego. Jest to kompleksowa obsługa prawna, obejmująca: doradztwo prawne
+i sporządzanie opinii prawnych, rejestrację przedsiębiorstw, sporządzanie i opiniowanie umów,
+prowadzenie sporów sądowych, udział w negocjacjach oraz windykację wierzytelności.</p>
+        </div>
       </div>
     </section>
   )
