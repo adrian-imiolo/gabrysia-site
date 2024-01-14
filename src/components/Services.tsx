bd192c128f60f1d131628bddb9c94735d6151d2c
Patryk <96724531+patryk-reba@users.noreply.github.com>
2024-01-14 18:27:34 +0100

    services

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -2,13 +2,29 @@ import { servicesData } from "./servicesData";
 
 function ServicesDetails() {
   return (
-    <div className="services-details content-container">
+    <div className="content-container">
       {servicesData.map((service, index) => (
-        <div className="services-detail-item" key={index} id={service.path}>
-          <img src={service.img1} />
-          <h3>{service.name}</h3>
-          <p>{service.title}</p>
-          <p>{service.content}</p>
+        <div key={index} id={service.path} className="services-details">
+          {index % 2 == 0 && (
+            <div className="services-detail-img">
+              <img src="https://picsum.photos/200/300" />
+            </div>
+          )}
+
+          <div className="services-detail-img-mobile">
+            <img src="https://picsum.photos/200/300" />
+          </div>
+
+          <div className="services-detail-item">
+            <h3>{service.name}</h3>
+            <p>{service.title}</p>
+            <p>{service.content}</p>
+          </div>
+          {index % 2 !== 0 && (
+            <div className="services-detail-img">
+              <img src="https://picsum.photos/200/300" />
+            </div>
+          )}
         </div>
       ))}
     </div>
